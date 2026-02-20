5ccc9f2563cd1417725727812bfa98bd3795c122
Lucas Calíope <lucascaliope2010@email.com>
2026-02-19 22:08:37 -0300

    fix apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Cart from "./components/Cart";
 import { GlobalStyle } from "./styles";
 import { AppRoutes } from "./routes/AppRoutes";
